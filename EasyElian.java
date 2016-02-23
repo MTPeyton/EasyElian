5542e9ae44099cf10b091b78db426c7ba77863c6
MTPeyton <matt.t.peyton@gmail.com>
2016-02-23 15:28:03 -0500

    Started file with a TODO list

---
+++ EasyElian.java
@@ -0,0 +1,12 @@
+public class Assig2 {
+	public static void main(String [] args) {
+		/*Todo
+		
+		1. Setup a GUI with two opposing textboxes
+		2. basic font for textbox on the left, make it editable
+		3. textbox on the right is not editable but IS copyable
+		4. textbox on the right mirrors the text written in the one of the left
+		5. font of the right textbox is the elianscript font (this will be hard!!!)
+		*/
+	}
+}
